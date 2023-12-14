8302eebd3e79fa04a8bfc880c41cc0465dc37e8f
Álvaro Torres <alvaro.espindola.torres@gmail.com>
2023-12-13 22:56:28 -0600

    (navbar):implementación de reactividad

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,61 +1,68 @@
-import {React, useState, useEffect} from 'react'
-import './Navbar.css'
-import iconDev from "../../assets/dev.png";
+import { React, useState, useEffect } from 'react';
+import './Navbar.css';
 
 function Navbar() {
-
   const elementosNav = [
-    {propiedad: "Inicio", clase: "principal"},
-    {propiedad: "Sobre mí", clase: "about"},
-    {propiedad: "Proyectos", clase: "projects"},
-    {propiedad: "Certificados", clase: "certifications"}
-  ]
+    { propiedad: 'Inicio', clase: 'principal' },
+    { propiedad: 'Sobre mí', clase: 'about' },
+    { propiedad: 'Proyectos', clase: 'projects' },
+    { propiedad: 'Certificados', clase: 'certifications' },
+  ];
 
-  //* Métodos para los items de la navbar
-  const changeState = (elemento) =>{
-    disabledAnexos();
-      const anexoClickeado = document.querySelector(`.${elemento}`);
-      anexoClickeado.classList.add("seleccionado");    
-  }
+  const [activeSection, setActiveSection] = useState('');
 
-  const disabledAnexos = () => {
-    const anexos = document.querySelectorAll('.navbar-list_item');
-    for (const anexo of anexos) {
-      anexo.classList.remove("seleccionado");
-    }
-  }
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY;
+    const sections = document.querySelectorAll('.section');
 
-  const [displayResolutions, setDisplayResolutions] = useState(window.innerWidth);
+    let closestSection = '';
+    let closestDistance = Number.MAX_VALUE;
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+      const windowHeight = window.innerHeight;
+
+      const distanceToCenter = Math.abs(scrollPosition - sectionTop + sectionHeight * 0.5 - windowHeight * 0.5);
+
+      if (distanceToCenter < closestDistance) {
+        closestDistance = distanceToCenter;
+        closestSection = section.id;
+      }
+    });
+
+    setActiveSection(closestSection);
+  };
 
   useEffect(() => {
-    setDisplayResolutions(window.innerWidth);
-  }, [displayResolutions]); 
+    const timer = setTimeout(() => {
+      handleScroll(); // Calcular la sección activa después de un breve intervalo
+    }, 200); // Ajustar el tiempo según sea necesario para permitir que el DOM se estabilice
 
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      clearTimeout(timer);
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
-    displayResolutions > 768 ? (
-      <nav className='navbar'>
-        <div className="navbar-icondev">
-          <img className='icondev' src={iconDev} alt="" />
-        </div>
-  
-        <div className="navbar-items">
-          <ul className='navbar-list'>
-            {
-              elementosNav.map(elemento => {
-                const { propiedad, clase } = elemento;
-                return (
-                  <li className={`navbar-list_item ${clase}`} onClick={() => changeState(clase)}>
-                    <a className='ref' href={`#${clase}`}>{propiedad}</a>
-                  </li>
-                )
-              })
-            }
-          </ul>
-        </div>
-      </nav>
-    ) : null
-  )
+    <nav className='navbar'>
+      <div className='navbar-items'>
+        <ul className='navbar-list'>
+          {elementosNav.map((elemento) => {
+            const { propiedad, clase } = elemento;
+            return (
+              <li key={propiedad} className={`navbar-list_item ${activeSection === clase ? 'seleccionado' : ''}`}>
+                <a className='ref' href={`#${clase}`}>{propiedad}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
